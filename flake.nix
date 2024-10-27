8b143d7602ea1814120d8852d89b30ccfabce131
Diogo Gaspar <dgaspar@kth.se>
2024-10-27 23:38:26 +0100

    feat: add maven, yarn, pnpm to flake

--- flake.nix
+++ flake.nix
@@ -22,6 +22,8 @@
 
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell { packages = [ pythonEnv ]; };
+      devShells.x86_64-linux.default = pkgs.mkShell { packages = [
+        pythonEnv pkgs.maven pkgs.yarn pkgs.pnpm
+      ]; };
     };
 }
