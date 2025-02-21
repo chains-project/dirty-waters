be36d16bba900b2e58c45e705733f956d92060ff
Diogo Gaspar <dgaspar@kth.se>
2025-02-21 14:28:12 +0100

    fix: changing max_retries to 3

--- tool/static_analysis.py
+++ tool/static_analysis.py
@@ -255,7 +255,7 @@ def check_parent_scm(package):
             for location in parent_scm_locations:
                 if location:
                     repo_api, simplified_path, package_full_name, _, _, _ = api_constructor(package, location)
-                    data = make_github_request(repo_api, max_retries=2)
+                    data = make_github_request(repo_api, max_retries=3)
                     if data:
                         stopping = True
                         existing_scm_data = data
@@ -288,7 +288,7 @@ def check_existence(package_name, repository, package_manager):
     open_issues_count = None
     status_code = 404
 
-    data = make_github_request(repo_api, max_retries=2)
+    data = make_github_request(repo_api, max_retries=3)
     parent_scm_result = {}
     if not data:
         if package_manager == "maven":
